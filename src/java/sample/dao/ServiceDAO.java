d3d1b17f227042fe2f21101638d516a1cad56e79
ThanhNam <trannam.nt865@gmail.com>
2023-05-30 22:06:13 +0700

    getListServiceTest

--- src/java/sample/dao/ServiceDAO.java
+++ src/java/sample/dao/ServiceDAO.java
@@ -1,14 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sample.dao;
 
-/**
- *
- * @author MSI AD
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import sample.utils.Utils;
+import sample.dto.ServiceDTO;
+
 public class ServiceDAO {
+    
+    private static final String querryService = "select * from tbl_Service ";
+    
+    public static List<ServiceDTO> serviceWithOutStart() throws SQLException, ClassNotFoundException{
+        List<ServiceDTO> listService = null;
+        ServiceDTO ser = null;
+        Connection conn = null;
+        PreparedStatement ptm = null;
+        ResultSet rs = null;
+        try {
+            conn = Utils.getConnection();
+            if (conn != null) {
+                ptm = conn.prepareStatement(querryService);
+                rs = ptm.executeQuery();
+                while (rs.next()) {
+                    String ser_id = rs.getString("service_id");
+                    String ser_name = rs.getString("service_name");
+                    int fee = rs.getInt("fee");
+                    String icon_link = rs.getString("icon_link");
+                    boolean status = rs.getBoolean("status");
+                    ser = new ServiceDTO(ser_id, ser_name, fee, icon_link, status);
+                    listService.add(ser);
+                }
+            }
+        } catch (SQLException e) {
+
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return listService;   
+    }
+    
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        List<ServiceDTO> list = serviceWithOutStart();
+        int i=list.size();
+        System.out.println(i);
+        
+    }
     
 }
