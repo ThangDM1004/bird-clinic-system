d0f6c3127307a51208563a37ebe6e49c6e16dba8
Dang Minh Thang <dmthang.longan@gmail.com>
2023-06-02 23:45:20 +0700

    fix

--- src/java/sample/dao/UserDAO.java
+++ src/java/sample/dao/UserDAO.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -181,5 +182,37 @@ public class UserDAO {
     public static void main(String[] args) {
         UserDAO dao = new UserDAO();
         System.out.println(dao.Login("doctor1", "1"));
+    }
+    private static final String LOGIN_GOOGLE = "SELECT email FROM tbl_Account WHERE email = ?";
+    public UserDTO checkLogin(String email) throws SQLException {
+        UserDTO user = null;
+        Connection conn = null;
+        PreparedStatement ptm = null;
+        ResultSet rs = null;
+        try {
+            conn = Utils.getConnection();
+            if (conn != null) {
+                ptm = conn.prepareStatement(LOGIN_GOOGLE);
+                ptm.setString(1, email);
+                rs = ptm.executeQuery();
+                if (rs.next()) {
+                    String email_ = rs.getString("email");
+                   user = new UserDTO("", "", email, "", null, "", "", "", "", true, "");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return user;
     }
 }
