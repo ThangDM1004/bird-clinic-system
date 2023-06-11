d07b8fcc8facf8aa3d8a3a2003e8adc439b851a0
hieu15402 <100761394+hieu15402@users.noreply.github.com>
2023-06-11 13:02:06 +0700

    update-hieu
    update-hieu

--- src/java/sample/controller/MainController.java
+++ src/java/sample/controller/MainController.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@MultipartConfig
 public class MainController extends HttpServlet {
 
     private static String LOGIN = "login";
