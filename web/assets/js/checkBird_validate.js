d773be7a50c22d69c38b294a23a3b59f9aa2d58e
Dang Minh Thang <dmthang.longan@gmail.com>
2023-06-16 13:28:45 +0700

    thang xong tuan nay

--- web/assets/js/checkBird_validate.js
+++ web/assets/js/checkBird_validate.js
@@ -1,26 +1,36 @@
 jQuery(document).ready(function () {
     var isBirdnameAvailable = false;
-     var isBirdImgAvailable = false;
+    var isBirdImgAvailable = false;
     
-    
+    function enableSubmitButton() {
+        if (isBirdnameAvailable === true && isBirdImgAvailable === true) {
+            $('#submit').prop('disabled', false);
+        } else if (isBirdnameAvailable === true && isBirdImgAvailable === false) {
+            $('#submit').prop('disabled', true);
+        }else{
+              $('#submit').prop('disabled', true);
+        }
+    }
+
+
     $('#birdname').change(function () {
         var birdname = $('#birdname').val();
-          var username = $('#username').val();
+        var username = $('#username').val();
         $.ajax({
             type: 'POST',
-            data: {birdname: birdname, username : username},
+            data: {birdname: birdname, username: username},
             url: 'BirdnameCheckController',
             success: function (result) {
                 $('#result').html(result);
 
                 if (result === 'Birdname is available to use') {
                     $('#result').removeClass().addClass('available');
-                    isUsernameAvailable = true;
+                    isBirdnameAvailable = true;
                 } else if (result === 'This birdname has been taken, try another') {
                     $('#result').removeClass().addClass('already-exists');
-                    isUsernameAvailable = false;
+                    isBirdnameAvailable = false;
                 }
-               
+                    enableSubmitButton();
             }
         });
     });
@@ -35,11 +45,12 @@ jQuery(document).ready(function () {
                 $('#result_1').html(result_1);
                 if (result_1 === 'Image is available to use') {
                     $('#result_1').removeClass().addClass('available');
-                    isEmailAvailable = true;
+                    isBirdImgAvailable = true;
                 } else if (result_1 === 'Allowed JPG, GIF or PNG., try another') {
                     $('#result_1').removeClass().addClass('already-exists');
-                    isEmailAvailable = false;
+                    isBirdImgAvailable = false;
                 }
+                enableSubmitButton();
             }
         });
     });
